63414665d0354d95ea0a28c8582cd19ea5e8d78e
Adam Gorring <adamgorring@gmail.com>
2022-03-20 23:09:19 +1000

    Initial Commit

---
+++ Shared/workoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutApp.swift
+//  Shared
+//
+//  Created by Adam Gorring on 20/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct workoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
